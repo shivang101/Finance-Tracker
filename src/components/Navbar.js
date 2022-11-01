eaca93813c8b5bf17af840f3bf74fd2076738e88
shivang <shivangmathur101@gmail.com>
2022-11-01 23:06:38 +0530

    added conditional showing of content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
-
+import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
 
 import styles from "./Navbar.module.css";
 export default function Navbar() {
   const { logout, error, isPending } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
@@ -12,17 +13,26 @@ export default function Navbar() {
         <li className={styles.title}>
           <NavLink to="/home"> myMoney</NavLink>
         </li>
-        <li>
-          <NavLink to="/login"> Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/signup"> Sign-up</NavLink>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            Logout
-          </button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <NavLink to="/login"> Login</NavLink>
+            </li>
+            <li>
+              <NavLink to="/signup"> Sign-up</NavLink>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>Hello, {user.displayName}</li>
+            <li>
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
